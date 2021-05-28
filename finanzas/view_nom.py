6375f7241f876d9e646af6c4cf13cd82dc76e2f0
Valentin Reyes <vreyesr@jupiter.subnet05241558.vcn05241558.oraclevcn.com>
2021-05-28 20:20:18 +0000

    Updated with more data

--- finanzas/view_nom.py
+++ finanzas/view_nom.py
@@ -32,22 +32,43 @@ def viewnom(file_name):
     emisor =  root[0] 
     receptor =  root[1]
     nomina = root[3][0]
+    num_empleado = root[3][0][1].get('NumEmpleado')
+    fecha_inicial = nomina.get('FechaInicialPago')
+    fecha_final = nomina.get('FechaFinalPago')
+    dias = nomina.get('NumDiasPagados')
     
+    total = []
+    headers = ['# Emp', 'F. Inicial', 'F. Final', 'Dias', 'Percepciones', 'Deducciones', 'Concepto', 'Clave']
     print(emisor.get('Nombre'), emisor.get('Rfc'))  
     print(nomina.get('FechaInicialPago'), nomina.get('FechaFinalPago'), nomina.get('NumDiasPagados'))
     print(nomina.get('TotalDeducciones'), nomina.get('TotalPercepciones'))  
 
-    print("Percepciones")
+    #print("Percepciones")
     for k in range(len(root[3][0][2])):
-        print(root[3][0][2][k].get('Concepto'), root[3][0][2][k].get('ImporteExento'), root[3][0][2][k].get('ImporteGravado'))
+       importe_gravado = float(root[3][0][2][k].get('ImporteGravado'))
+       importe_exento = float(root[3][0][2][k].get('ImporteExento'))
+       if float(root[3][0][2][k].get('ImporteGravado')) > 0:
+           total.append((num_empleado, fecha_inicial, fecha_final, dias, "{:,.2f}".format(importe_gravado), '-', root[3][0][2][k].get('Concepto').split('_')[1], root[3][0][2][k].get('Concepto').split('_')[0]))
+       if float(root[3][0][2][k].get('ImporteExento')) > 0:
+           total.append((num_empleado, fecha_inicial, fecha_final, dias, "{:,.2f}*".format(importe_exento), '-', root[3][0][2][k].get('Concepto').split('_')[1], root[3][0][2][k].get('Concepto').split('_')[0]))
+    #print(tabulate(total))
 
-    print("Deducciones")
+    #print("Deducciones")
     for k in range(len(root[3][0][3])):
-        print(root[3][0][3][k].get('Concepto'), root[3][0][3][k].get('Importe'))
+        importe = float(root[3][0][3][k].get('Importe'))
+        #print(('-', root[3][0][3][k].get('Importe'), root[3][0][3][k].get('Concepto').split('_')[1], root[3][0][3][k].get('Concepto').split('_')[0] ))
+        total.append((num_empleado, fecha_inicial, fecha_final, dias, '-', "{:,.2f}".format(importe), root[3][0][3][k].get('Concepto').split('_')[1], root[3][0][3][k].get('Concepto').split('_')[0] ))
+    #print(tabulate(total))
 
-    print("Otro Pagos")
+    #print("Otro Pagos")
     for k in range(len(root[3][0][4])):
-        print(root[3][0][4][k].get('Concepto'), root[3][0][4][k].get('Importe'))
+        otro_importe = float(root[3][0][4][k].get('Importe'))
+#        print(root[3][0][4][k].get('Concepto'), root[3][0][4][k].get('Importe'))
+        total.append((num_empleado, fecha_inicial, fecha_final, dias, "{:,.2f}".format(otro_importe), '-',  root[3][0][4][k].get('Concepto').split('_')[1], root[3][0][4][k].get('Concepto').split('_')[0]))
+    #print(tabulate(total, floatfmt=("","","",",.2f",",.2f"), colalign=("","","","right","right","right")))
+    #print(tabulate(total, floatfmt=("","","",",.2f",",.2f"), colalign=("","","","right","right","right")))
+    #print(tabulate(sorted(total, key=lambda x: x[6]), floatfmt=".3f", numalign="right"))
+    print(tabulate(sorted(total, key=lambda x: x[7]), stralign="right", headers= headers))
 
 
 
